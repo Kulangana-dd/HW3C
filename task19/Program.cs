d33f8191e258dea3421c8d5478faa76b5213dca1
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-09-28 18:33:02 +0300

    task 19

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 19 Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+int GetReverseNumber(int number)
+{
+    int reverseNumber = 0;
+    while(number > 0)
+    {
+        reverseNumber = (reverseNumber * 10) + (number % 10);
+        number = number / 10;
+    }  
+    return reverseNumber;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 9999 & number < 100000)
+{
+    if(GetReverseNumber(number) == number) 
+    {
+    Console.WriteLine("Это палиндром");
+    }
+    else Console.WriteLine("Это не палиндром");
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
